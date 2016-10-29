8ab8719ca12d8187aa07fc5f7b162b1dd3c72d6c
Ryan Munger <ryanmunger@gmail.com>
2016-10-29 09:33:59 -0400

    Add build step into Procfile

--- src/views/AirControlsView.js
+++ src/views/AirControlsView.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class AirControlsView extends Component {
     render () {
         return (
-            <p>Air Controls View</p>
+            <p>Air Controls View&amp;</p>
         );
     }
 }
