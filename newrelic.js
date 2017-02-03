175224c2f1b912154aa71f5f99bb89ebda9bac52
Ryan Munger <ryanmunger@gmail.com>
2017-02-03 15:27:22 -0800

    Use process.env for newly generated new relic license key

--- newrelic.js
+++ newrelic.js
@@ -14,7 +14,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '74cbc6ac15d78a5015f1b55c9c8cc60659db4046',
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
